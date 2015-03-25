919498467ac3e204ae2750c767cada634e7c9cfb
Robert J. Schofield <rjschof@vt.edu>
2015-03-24 23:13:10 -0400

    Initial commit.

---
+++ src/org/awesomeco/trianglesmash/TriangleSmashScreen.java
@@ -0,0 +1,29 @@
+package org.awesomeco.trianglesmash;
+
+import android.widget.TextView;
+import sofia.app.ShapeScreen;
+
+/**
+ * The TriangleSmashScreen class handles all of the operations necessary for
+ * the view to act properly.
+ *
+ * @author Lauren Malhotra (laurcm6)
+ * @author Robert Schofield (rjschof)
+ * @author Adam Zelenka (zadam7)
+ * @version 2015.03.23
+ */
+public class TriangleSmashScreen extends ShapeScreen
+{
+    private TextView gameStatus;
+
+    /**
+     * Initializes the screen.
+     */
+    public void initialize()
+    {
+        gameStatus.setText("Game initialized successfully.");
+        System.out.println("Test.");
+    }
+
+
+}
