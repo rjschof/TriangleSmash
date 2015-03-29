4f38f269bb4596c445232f5d9329ae93f7bd3743
Robert J. Schofield <rjschof@vt.edu>
2015-03-28 21:02:14 -0400

    Added a test class for the screen.
    Added a test class that will test TriangleSmashScreen.

---
+++ src/org/awesomeco/trianglesmash/TriangleSmashScreenTest.java
@@ -0,0 +1,34 @@
+package org.awesomeco.trianglesmash;
+
+import student.AndroidTestCase;
+
+// -------------------------------------------------------------------------
+/**
+ *  Tests for the TriangleSmashScreen class.
+ *
+ * @author Lauren Malhotra (laurcm6)
+ * @author Robert Schofield (rjschof)
+ * @author Adam Zelenka (zadam7)
+ * @version 2015.03.23
+ */
+
+public class TriangleSmashScreenTest
+    extends AndroidTestCase<TriangleSmashScreen>
+{
+    /**
+     * Constructor for the TriangleSmashScreenTest class.
+     */
+    public TriangleSmashScreenTest(String activityClassName)
+    {
+        super(activityClassName);
+    }
+
+    /**
+     * Sets up the test case.
+     */
+    public void setUp()
+    {
+        // TODO
+    }
+
+}
