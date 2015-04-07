e50b8a54624d8cfc541b507eca84b538e3ea6269
Robert J. Schofield <rjschof@vt.edu>
2015-04-07 11:46:05 -0400

    Revert "Changed a little of TriangleShape."
    This reverts commit 75b6815459558fdc31022568fa1399c7394328d0.

--- src/org/awesomeco/trianglesmash/TriangleShape.java
+++ src/org/awesomeco/trianglesmash/TriangleShape.java
@@ -39,8 +39,6 @@ public class TriangleShape extends FillableShape
     private float right;
     private float bottom;
 
-    private Polygon polygon;
-
     private float distanceX;
     private float distanceYTop;
     private float distanceYBottom;
@@ -101,7 +99,6 @@ public class TriangleShape extends FillableShape
             PointF origin = getPosition();
             Polygon tri = new Polygon(left + Math.abs((right-left)/2), top,
                 left, bottom, right, bottom);
-            this.polygon = tri;
             getFill().fillPolygon(drawing, getAlpha(), tri,
                 origin);
         }
@@ -151,16 +148,26 @@ public class TriangleShape extends FillableShape
     /**
      * Retrieves the bounding box for this shape.
      * TODO: Figure out how to really implement this.
+     * NOTICE: This method is entirely based off of the getBounds() method from
+     * OvalShape.
      * @return the bounding box for this shape
      */
     @Override
     public RectF getBounds()
     {
-        PointF origin = getPosition();
-        RectF bounds = polygon.getBounds();
+        // If the body has been created, update the center point using the
+        // body's current position.
 
-        bounds.offset(origin.x, origin.y);
-        return bounds;
+        PointF center = calculateCentroid();
+
+        Body b2Body = getB2Body();
+        if (b2Body != null)
+        {
+            Box2DUtils.vec2ToPointF(b2Body.getPosition(), center);
+        }
+
+        return new RectF(center.x - distanceX, center.y - distanceYTop,
+            center.x + distanceX, center.y + distanceYBottom);
     }
 
     // ----------------------------------------------------------
@@ -177,8 +184,8 @@ public class TriangleShape extends FillableShape
         float y = (top + bottom + bottom) / 3;
 
         distanceX = newBounds.width() / 2;
-        distanceYTop = Math.abs(newBounds.top - y);
-        distanceYBottom = Math.abs(newBounds.bottom - y);
+        distanceYTop = newBounds.top - y;
+        distanceYBottom = newBounds.bottom - y;
 
         recreateFixtures();
         conditionallyRepaint();
